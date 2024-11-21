d519193b9db1e6329ac803060dc16de4184b9a73
gunnelly <gunnelly.r@northeastern.edu>
2024-11-21 17:59:11 -0500

    Commit 1

---
+++ Assignment_2/src/model/Address.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Rohit Gunnelly
+ */
+public class Address {
+    private String streetAddress;
+    private String unitNumber;
+    private String state;
+    private String city;
+    private String zip;
+    private String phone;
+    
+    @Override
+    public String toString() {
+        return streetAddress + ", " + (unitNumber.isEmpty() ? "" : "Unit " + unitNumber + ", ") + city + ", " + state + " " + zip + ", Phone: " + phone;
+    }
+    
+    public Address(String streetAddress, String unitNumber, String city, String state, String zip, String phone) {
+        this.streetAddress = streetAddress;
+        this.unitNumber = unitNumber;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        
+        
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getUnitNumber() {
+        return unitNumber;
+    }
+
+    public void setUnitNumber(String unitNumber) {
+        this.unitNumber = unitNumber;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+}
